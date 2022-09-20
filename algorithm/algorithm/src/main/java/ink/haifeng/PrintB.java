1519b2c621030bf7f69a332177543e86768e080f
haifeng <fenglex@126.com>
2022-09-20 23:29:07 +0800

    提交位运算

--- algorithm/algorithm/src/main/java/ink/haifeng/PrintB.java
+++ algorithm/algorithm/src/main/java/ink/haifeng/PrintB.java
@@ -25,6 +25,7 @@ public class PrintB {
         printB(-1);
 
         printB(5);
+        printB(1<<2);
         // ~ 表示取反，就是将二进制中的0全部转为1,1全部转为0
         printB(~5);
 
@@ -32,11 +33,16 @@ public class PrintB {
         System.out.println((-5 & (1<<2)));
     }
 
+    /**
+     * 与运算如5与1<<2 进行与运算
+     * 00000000000000000000000000000101 (5)
+     * 00000000000000000000000000000100 (1<<2)
+     * 00000000000000000000000000000100 (两个数值计算与的结果)
+     * @param num
+     */
     private static void printB(int num) {
         for (int i = 31; i >= 0; i--) {
-            System.out.print(1<<i);
             System.out.print((num & (1 << i)) == 0 ? 0 : 1);
-            System.out.println("\t");
         }
         System.out.println();
     }
