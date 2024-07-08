572d76794f92c363333a14f0454958dcc160b8aa
haifeng <chenhaifeng@touguyun.cn>
2024-07-08 17:12:27 +0800

    获取磁盘温度

---
+++ disk-monitor/disk-monitor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 监控磁盘温度，读写量
+# @Time    : 2024/7/8 16:08
+# @Author  : haifeng
+# @File    : disk-monitor.py
+
+
+def get_temperature(disk):
+    """
+    huoqu 磁盘温度
+    :return:
+    """
+
+
+if __name__ == '__main__':
+    print(get_temperature('sda'))
